94ab1cdd3792e28add9bf55f8713ca1c71164d82
Attuah otabil <attuahotabilkay@gmail.com>
2023-04-30 17:56:06 +0000

    Define variables

---
+++ main.js
@@ -0,0 +1,5 @@
+const board = document.querySelector(".board");
+const black = document.querySelector(".black-button");
+const white = document.querySelector(".white-button");
+const random = document.querySelector(".random-button");
+const reset = document.querySelector(".reset-button"); 
