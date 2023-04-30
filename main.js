3cce52bfc262db0e3b926eab781322dcbb64e4c3
Attuah otabil <attuahotabilkay@gmail.com>
2023-04-30 18:05:29 +0000

    Add loop to creat square

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ const black = document.querySelector(".black-button");
 const white = document.querySelector(".white-button");
 const random = document.querySelector(".random-button");
 const reset = document.querySelector(".reset-button"); 
+
+for (let i = 0; i < 256; i++) {
+    board.style.gridTemplateColumns = "repeat(16, 1fr";board.style.gridTemplateRows = "repeat(16, 1fr";
+    let square = document.createElement("div");
+    square.style.background = "blue";
+    board.insertAdjacentElement("beforeend", square)
+}
